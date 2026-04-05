004e198e5a2d18efa998f021d57c123e8b2752bd
Sacol16 <santiago.colonia@uao.edu.co>
2026-04-05 14:06:00 -0500

    Update ApiConfig.cs

--- Assets/Scripts/Api/ApiConfig.cs
+++ Assets/Scripts/Api/ApiConfig.cs
@@ -3,5 +3,5 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Cartilla/API Config")]
 public class ApiConfig : ScriptableObject
 {
-    public string baseUrl = "http://localhost:3000";
+    public string baseUrl = "https://backend-cartilla-laboral-production.up.railway.app";
 }
